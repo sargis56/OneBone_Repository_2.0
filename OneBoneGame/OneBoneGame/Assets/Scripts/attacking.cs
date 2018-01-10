08300584f95cacd491c7fc56c7f230540c735186
Kristian Campbell <kriscampbell_94@yahoo.com>
2018-01-09 23:01:40 -0500

    Updated Content

--- OneBoneGame/OneBoneGame/Assets/Scripts/attacking.cs
+++ OneBoneGame/OneBoneGame/Assets/Scripts/attacking.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class attacking : MonoBehaviour {
 
+	public GameObject attackEffect;
+	public GameObject enemyExplosion;
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
 		if (collision.gameObject.tag == "enemy")
 		{
-			if (collision.gameObject.GetComponent<enemy>().hp <= 0)
-			{
-				Destroy(collision.gameObject);
-
-			}
-			Debug.Log("enemy : " +collision.gameObject.GetComponent<enemy>().hp);
+			Instantiate(attackEffect, collision.transform.position, Quaternion.identity);
+            if (collision.gameObject.GetComponent<enemy>().hp <= 0)
+            {
+                Instantiate(enemyExplosion, collision.transform.position, Quaternion.identity);
+                Destroy(collision.gameObject);
+            }
+            Debug.Log("enemy : " + collision.gameObject.GetComponent<enemy>().hp);
 		}
-	
-
 	}
-	
 }
--- OneBoneGame/OneBoneGame/Assets/Scripts/playerController.cs
+++ OneBoneGame/OneBoneGame/Assets/Scripts/playerController.cs
@@ -35,6 +35,7 @@ public class playerController : MonoBehaviour {
 
 	public bool attacking;
     public GameObject attackEffect;
+	public GameObject enemyExplosion;
     
 	public int isBonnie;
 	//ground check
@@ -176,7 +177,10 @@ public class playerController : MonoBehaviour {
             {
                 Instantiate(attackEffect, collision.transform.position, Quaternion.identity);
                 if (collision.gameObject.GetComponent<enemy>().hp <= 0)
+                {
+                    Instantiate(enemyExplosion, collision.transform.position, Quaternion.identity);
                     Destroy(collision.gameObject);
+                }
                 Debug.Log("enemy : " + collision.gameObject.GetComponent<enemy>().hp);
             }
 		}
@@ -206,24 +210,78 @@ public class playerController : MonoBehaviour {
 		switch (hp) 
 		{
 		case 60:
-			imageLifebar.sprite = spriteLifebar60;
-			break;
-		case 50:
-			imageLifebar.sprite = spriteLifebar50;
-			break;
-		case 40:
-			imageLifebar.sprite = spriteLifebar40;
-			break;
-		case 30:
-			imageLifebar.sprite = spriteLifebar30;
-			break;
-		case 20:
-			imageLifebar.sprite = spriteLifebar20;
-			break;
-		case 10:
-			imageLifebar.sprite = spriteLifebar10;
-			break;
-		case 0:
+                imageLifebar.sprite = spriteLifebar60;
+                break;
+            case 59:
+            case 58:
+            case 57:
+            case 56:
+            case 55:
+            case 54:
+            case 53:
+            case 52:
+            case 51:
+            case 50:
+                imageLifebar.sprite = spriteLifebar50;
+                break;
+            case 49:
+            case 48:
+            case 47:
+            case 46:
+            case 45:
+            case 44:
+            case 43:
+            case 42:
+            case 41:
+            case 40:
+                imageLifebar.sprite = spriteLifebar40;
+                break;
+            case 39:
+            case 38:
+            case 37:
+            case 36:
+            case 35:
+            case 34:
+            case 33:
+            case 32:
+            case 31:
+            case 30:
+                imageLifebar.sprite = spriteLifebar30;
+                break;
+            case 29:
+            case 28:
+            case 27:
+            case 26:
+            case 25:
+            case 24:
+            case 23:
+            case 22:
+            case 21:
+            case 20:
+                imageLifebar.sprite = spriteLifebar20;
+                break;
+            case 19:
+            case 18:
+            case 17:
+            case 16:
+            case 15:
+            case 14:
+            case 13:
+            case 12:
+            case 11:
+            case 10:
+            case 9:
+            case 8:
+            case 7:
+            case 6:
+            case 5:
+            case 4:
+            case 3:
+            case 2:
+            case 1:
+                imageLifebar.sprite = spriteLifebar10;
+                break;
+            case 0:
 			imageLifebar.sprite = spriteLifebar0;
 			break;
 		default:
